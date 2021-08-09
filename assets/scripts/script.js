d4d78754257cc2caadb07c0e572d3b2bd8fc269e
Henrique Luiz <hnrq.luiz1@gmail.com>
2021-08-08 23:51:21 -0300

    add: login page design

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,28 @@
 "use strict";
 
 import { createElem, modifyElem } from "./helpers.js";
+
+// login page
+const $usernameInput = document.getElementById("username-input");
+const $passwordInput = document.getElementById("password-input");
+const $loginBtn = document.querySelector(".login--main-form-button");
+
+$usernameInput.addEventListener("keyup", () => {
+  if($usernameInput.value && $passwordInput.value) {
+    $loginBtn.className = "login--main-form-button active";
+    $loginBtn.disabled = false;
+  } else {
+    $loginBtn.className = "login--main-form-button disabled";
+    $loginBtn.disabled = true;
+  }
+})
+
+$passwordInput.addEventListener("keyup", () => {
+  if($usernameInput.value && $passwordInput.value) {
+    $loginBtn.className = "login--main-form-button active";
+    $loginBtn.disabled = false;
+  } else {
+    $loginBtn.className = "login--main-form-button disabled";
+    $loginBtn.disabled = true;
+  }
+})
