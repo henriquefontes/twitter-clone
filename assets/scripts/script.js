059b7f13a2b0ea56dc18ed8d4235cbac9563a2b0
Henrique Luiz <hnrq.luiz1@gmail.com>
2021-08-11 13:05:50 -0300

    add: home page layout

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,53 +2,133 @@
 
 import { createElem, modifyElem } from "./helpers.js";
 
-// login container
-const $lgnUserInput = document.getElementById("username-login-input");
-const $lgnPasswordInput = document.getElementById("password-login-input");
+const registerSections = [
+  {
+    step: 1,
+    section: "register--section-account",
+  },
+  {
+    step: 2,
+    section: "register--section-photo",
+    storage: {
+      src: ""
+    }
+  },
+  {
+    step: 3,
+    section: "register--section-bio",
+  },
+  {
+    step: 4,
+    section: "register--section-username",
+  }
+]
+
+const userStorage = {
+  username: "",
+  name: "",
+  email: "",
+  password: "",
+  picture: "",
+  bio: ""
+}
+
+const setSection = (old, next) => {
+  const oldSection = document.querySelector(`.${old}`);
+  const nextSection = document.querySelector(`.${next}`);
+
+  oldSection.className = `${old} none`;
+  nextSection.className = next;
+}
+
+const setStorage = () => {
+
+}
+
+const finishRegister = () => {
+  document.location.href = "home.html";
+}
+
+const stepButtons = document.querySelectorAll("[data-next-step]")
+  .forEach(button => {
+    const sectionIndex = button.getAttribute("data-next-step") - 1;
+    const isLastStep = sectionIndex == registerSections.length;
+
+    if (isLastStep) {
+      button.addEventListener("click", () => {
+        finishRegister();
+      })
+
+      return
+    }
+
+    const oldSection = registerSections[sectionIndex - 1].section;
+    const nextSection = registerSections[sectionIndex].section;
+    
+    button.addEventListener("click", () => {
+      setSection(oldSection, nextSection);
+    })
+  });
+
+//
+
 const $loginBtn = document.getElementById("login-button");
+const $lgnUserInput = document.querySelector("[data-input='username-login']");
+const $lgnPasswordInput = document.querySelector("[data-input='password-login']");
 
 $lgnUserInput.addEventListener("keyup", () => {
-  if($lgnUserInput.value && $lgnPasswordInput.value) {
+  const twoInputTyped = $lgnUserInput.value && $lgnPasswordInput.value;
+
+  if (twoInputTyped) {
     $loginBtn.className = "form-button active";
     $loginBtn.disabled = false;
   } else {
     $loginBtn.className = "form-button disabled";
     $loginBtn.disabled = true;
   }
-});
+  
+})
 
 $lgnPasswordInput.addEventListener("keyup", () => {
-  if($lgnUserInput.value && $lgnPasswordInput.value) {
+  const twoInputTyped = $lgnUserInput.value && $lgnPasswordInput.value;
+
+  if(twoInputTyped) {
     $loginBtn.className = "form-button active";
     $loginBtn.disabled = false;
   } else {
     $loginBtn.className = "form-button disabled";
     $loginBtn.disabled = true;
   }
+
 });
 
 //register container
-const $rgUsernameInput = document.getElementById("username-register-input");
-const $rgEmailInput = document.getElementById("email-register-input");
+const $rgUsernameInput = document.querySelector("[data-input='name-register']");
+const $rgEmailInput = document.querySelector("[data-input='email-register']");
 const $registerBtn = document.getElementById("register-button");
 
 $rgUsernameInput.addEventListener("keyup", () => {
-  if($rgUsernameInput.value && $rgEmailInput.value) {
+  const twoInputTyped = $rgUsernameInput.value && $rgEmailInput.value;
+
+  if (twoInputTyped) {
     $registerBtn.className = "form-button active";
     $registerBtn.disabled = false;
   } else {
     $registerBtn.className = "form-button disabled";
     $registerBtn.disabled = true;
   }
+
 });
 
 $rgEmailInput.addEventListener("keyup", () => {
-  if($rgUsernameInput.value && $rgEmailInput.value) {
+  const twoInputTyped = $rgUsernameInput.value && $rgEmailInput.value;
+
+  if (twoInputTyped) {
     $registerBtn.className = "form-button active";
     $registerBtn.disabled = false;
   } else {
     $registerBtn.className = "form-button disabled";
     $registerBtn.disabled = true;
   }
-});
 
+});
